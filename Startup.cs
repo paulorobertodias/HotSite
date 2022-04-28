012daaaaaf86810c2eebe4b0388e2df77a5f0b5f
paulorobertodias <roberto-dias@bm.rs.gov.br>
2022-04-28 15:39:38 -0300

    Alterações no projeto

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Hotsite
 {
@@ -26,7 +27,7 @@ namespace Hotsite
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory LogFac)
         {
             if (env.IsDevelopment())
             {
@@ -48,6 +49,8 @@ namespace Hotsite
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            LogFac.AddFile("Logs/Log-{Date}.txt",LogLevel.Warning);
         }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function abrirAgenda(){
+    document.getElementById('popup_agenda').style.display = 'block';
+}
+function fecharAgenda(){
+    document.getElementById('popup_agenda').style.display =  'none';
+}
+
+function abrirDicas(){
+    document.getElementById('popup_dicas').style.display = 'block';
+}
+function fecharDicas(){
+    document.getElementById('popup_dicas').style.display =  'none';
+}
+
+
+function abrirApoio(){
+    document.getElementById('popup_apoio').style.display = 'block';
+}
+function fecharApoio(){
+    document.getElementById('popup_apoio').style.display =  'none';
+}
